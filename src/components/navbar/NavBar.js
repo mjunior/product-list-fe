6bbd23774db0953b05c8ced76a9728bcc01c62fb
mjunior <mauriciojs.junior@gmail.com>
2019-04-02 09:17:40 -0300

    Implementa funcionalidade de busca

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom'
+import { withRouter } from 'react-router-dom';
 import {
-  Container,
   Row,
-  Col
+  Col,
+  Container
 } from 'reactstrap';
 
 import logo from '../../assets/images/Logo_ML.png';
@@ -10,29 +12,54 @@ import ic_Search from '../../assets/images/ic_Search.png';
 import './navbar.css';
 
 class NavBar extends Component {
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      value: '',
+      onSearching: false
+    };
+    const { router } = this.props
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event){
+    this.setState({ value: event.target.value });
+  }
+  
+  handleSubmit(event){
+    event.preventDefault();
+    const value = this.state.value;
+    this.props.history.push(`/items?search=${value}`);
+  }
+  
   render() {
     return (
       <header>
-        <div class="container">
+        <Container>
           <Row className="navbar navbar-expand-lg navbar-light">
             <Col xs={{ size: 1, offset: 0 }} sm={{ size: 1, offset: 1 }}>
               <img src={logo} />
             </Col>
             <Col xs={{ size: 9 }} sm={{ size: 9 }}>
-              <div class="input-group">
-                <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-                <div class="input-group-append">
-                  <button class="btn btn-light" type="button">
-                    <img src={ic_Search} />
-                  </button>
+              <form onSubmit={this.handleSubmit}>
+                <div className="input-group">
+                  <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" value={this.state.value} onChange={this.handleChange} />
+                  <div className="input-group-append">
+                    <button type="submit" className="btn btn-light">
+                      <img src={ic_Search} />
+                    </button>
+                  </div>
                 </div>
-              </div>
+              </form>
             </Col>
           </Row>
-        </div>
+        </Container>
       </header>
     );
   }
 }
 
-export default NavBar;
+export default withRouter(NavBar);
